4fe69b8bff897f66f328afb6573585924be851d2
alspxlj <shangbu167481@126.com>
2021-04-26 15:27:02 +0800

    practicum2 aangepast

--- src/week1/les02/practicum2/Voetbalclub.java
+++ src/week1/les02/practicum2/Voetbalclub.java
@@ -13,57 +13,19 @@ public class Voetbalclub {
 		this.clubNaam = clubNaam;
 	}
 
-	public int getAantalGewonnen() {
-		return aantalGewonnen;
-	}
-
-	public void setAantalGewonnen(int aantalGewonnen) {
-		this.aantalGewonnen = aantalGewonnen;
-	}
-
-	public int getAantalVerloren() {
-		return aantalVerloren;
-	}
-
-	public void setAantalVerloren(int aantalVerloren) {
-		this.aantalVerloren = aantalVerloren;
-	}
-
-	public char getCh() {
-		return ch;
-	}
-
-	public void setCh(char ch) {
-		this.ch = ch;
-	}
-
-	public String getClubNaam() {
-		return clubNaam;
-	}
-
 	public void verwerkResultaat(char ch) { 
 		if (ch == 'w') {
 			aantalGewonnen = aantalGewonnen + 1;
-			}
+		}
 		if (ch == 'g') {
 			aantalGelijk = aantalGelijk + 1;
-			}
+		}
 		
 		if (ch == 'v') { 
 			aantalVerloren = aantalVerloren + 1; 
-			} 
-	}
-
-	public int aantalGespeeld(){
-		int totaleGespeeldeWedstrijden = aantalGewonnen + aantalVerloren + aantalGelijk;
-		return totaleGespeeldeWedstrijden;
-	}
-
-	public int aantalPunten(){
-		int gewonnenPunten = aantalGewonnen * 3;
-		int gelijkPunten = aantalGelijk * 1;
-		totalePunten = totalePunten + gewonnenPunten + gelijkPunten;
-		return totalePunten;
+		}
+		totaleGespeeldeWedstrijden = aantalGewonnen + aantalVerloren + aantalGelijk;
+		totalePunten = (aantalGewonnen * 3) + (aantalGelijk * 1);
 	}
 
 	public String toString(){
